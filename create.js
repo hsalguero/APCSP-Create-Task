db58b59ba13ce9ac398c13dea37ff310f6afe162
hsalguero <helmersalg@gmail.com>
2019-03-13 10:52:47 -0400

    new pages and styling added

--- create.js
+++ create.js
@@ -1,30 +1,43 @@
 
 function brs(num) {
   let returnStr = ' '
-  for ( i = 0; i < num; i++){
+  for (i = 0; i < num; i++){
     returnStr = returnStr + '<br/>'
   }
   return returnStr
 }
+//
+
 function ait() {
-  let content = document.getElementById("contentdiv")
+  let content = document.getElementById('contentdiv')
 
 }
 function apa() {
-  let content = document.getElementById("contentdiv")
+  let content = document.getElementById('contentdiv')
 
 }
 function magnet() {
-  let content = document.getElementById("contentdiv")
+  let content = document.getElementById('contentdiv')
 
 }
 function uctech() {
-  let content = document.getElementById("contentdiv")
-
-}
-function aahs() {
-  text = "You walk into Allied and find a student collapsed on the floor. What will you do?"
-  let content = document.getElementById("contentdiv")
+  text = 'UCTECH has a variety of vocations to choose from. Which one will you choose?'
+  let content = document.getElementById('contentdiv')
   let brStr = brs(5);
   content.innerHTML = (brStr + text + brStr);
+}
+function aahs() {
+  location.href='aahs.html'
+}
+function aahs1() {
+    text = 'You walk into Allied and find a student collapsed on the floor. What will you do?'
+    let content = document.getElementById('contentdiv')
+    let brStr = brs(5);
+    content.innerHTML = (brStr + text + brStr);
+}
+function ignore() {
+  location.href='ignore.html'
+}
+function returnTitle() {
+  location.href='./index.html'
 }
